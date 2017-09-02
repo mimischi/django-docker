43bf836282c625e0a4c84894c67004684655292d
Michael Gecht <gecht.m@gmail.com>
2017-09-02 22:44:16 +0200

    Correct app name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangodocker.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -16,7 +16,6 @@ if __name__ == "__main__":
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
+                "forget to activate a virtual environment?")
         raise
     execute_from_command_line(sys.argv)
