6bfcdfbfa7a3b769dd55c71a7d2b4f8adba95212
Michael Gecht <gecht.m@gmail.com>
2017-09-02 23:28:20 +0200

    Add simple template to save a hit count

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Visits
+
+
+def home(request):
+    template_name = 'main/home.html'
+    context = {}
+
+    try:
+        c = Visits.objects.get(pk=1)
+    except Visits.DoesNotExist:
+        c = Visits(count=0)
+
+    c.count += 1
+    c.save()
+
+    context['count'] = c.count
+
+    return render(request, template_name, context=context)
--- settings/urls.py
+++ settings/urls.py
@@ -11,11 +11,14 @@ Class-based views
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+    2. Add, include('main.urls')),
+    url(r'^admin/', admin.site.urls),
+]"""
+
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('main.urls')),
     url(r'^admin/', admin.site.urls),
 ]
