c0585b7b77f6723300c9b0e39a36539e84482492
Michael Gecht <gecht.m@gmail.com>
2017-09-03 19:29:42 +0200

    Fix URL regex

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('main.urls')),
+    url(r'^', include('main.urls'), name='main'),
     url(r'^admin/', admin.site.urls),
 ]
