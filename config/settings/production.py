864978f9e0d91d23f27ac12fd784ce4d1745dee2
Michael Gecht <gecht.m@gmail.com>
2018-02-15 17:05:01 +0100

    Add Sentry support.

--- config/settings/production.py
+++ config/settings/production.py
@@ -32,12 +32,76 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ALLOWED HOSTS
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
+# Raven Sentry client
+# See https://docs.getsentry.com/hosted/clients/python/integrations/django/
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
+
 # APPS
 INSTALLED_APPS += ("gunicorn", )
 
 # DATABASE
 DATABASES = {'default': env.db("DATABASE_URL")}
 DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+# Sentry Configuration
+SENTRY_DSN = env('DJANGO_SENTRY_DSN')
+SENTRY_CLIENT = env(
+    'DJANGO_SENTRY_CLIENT',
+    default='raven.contrib.django.raven_compat.DjangoClient')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format':
+            '%(levelname)s %(asctime)s %(module)s '
+            '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class':
+            'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'django.security.DisallowedHost': {
+            'level': 'ERROR',
+            'handlers': ['console', 'sentry'],
+            'propagate': False,
+        },
+    },
+}
+SENTRY_CELERY_LOGLEVEL = env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO)
+RAVEN_CONFIG = {
+    'CELERY_LOGLEVEL': env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO),
+    'DSN': SENTRY_DSN
+}
 
 # ADMIN URL
 ADMIN_URL = env('DJANGO_ADMIN_URL')
